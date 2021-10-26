47229076e1ac40268a6fc37763199048db92ea2c
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-10-26 16:53:49 -0300

    Implementa cálculo de preço no carrinho

--- src/app/Components/cart-page/cart-page.component.ts
+++ src/app/Components/cart-page/cart-page.component.ts
@@ -10,6 +10,7 @@ import { CartProduct } from './../../Models/cartProduct.model';
 export class CartPageComponent implements OnInit {
   products:CartProduct[] = [];
   validSizes:string[] = [];
+  total:number = 0;
 
   constructor() { }
 
@@ -19,8 +20,8 @@ export class CartPageComponent implements OnInit {
     if(savedCart){
       this.products = JSON.parse(savedCart);
     }
-
     this.validSizes = ['P', 'M', 'G', 'GG']
+    this.calculateTotal()
   }
 
   clickIncrease(product:CartProduct){
@@ -29,21 +30,40 @@ export class CartPageComponent implements OnInit {
     } else {
       product.quantity++;
     }
+    this.calculateTotal()
   }
 
   clickDecrease(product:CartProduct){
     if(!product.quantity){
       product.quantity = 1
     } else {
-      product.quantity <= 0 ? product.quantity = 0 : product.quantity--;
+      product.quantity <= 1 ? product.quantity = 1 : product.quantity--;
     }
+    this.calculateTotal()
   }
 
   deleteProduct(product:CartProduct){
     this.products = this.products.filter(item => {
       item != product
     })
-    localStorage.setItem("estampas_products", JSON.stringify(this.products))
+    localStorage.setItem("estampas_products", JSON.stringify(this.products));
+    this.calculateTotal();
+
   }
 
+  updateSize(){
+    localStorage.setItem("estampas_products", JSON.stringify(this.products));
+  }
+
+  calculateTotal(){
+    this.total = this.products.reduce((total, next) => {
+      if(!next.quantity){
+        next.quantity = 0
+      }
+      if(!next.price){
+        next.price = 0
+      }
+      return total + (next.quantity * next.price)
+    }, 0)
+  }
 }
