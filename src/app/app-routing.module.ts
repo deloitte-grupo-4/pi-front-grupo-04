86e507f27d2e34f598d9e8a57800595431597e29
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-10-27 13:14:32 -0300

    Adiciona página de confirmação de pedido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
         }
 
       ],
-      // canActivate: [AuthenticationGuard]
+       canActivate: [AuthenticationGuard]
     },
     {
       path: '', component: AuthenticationComponent,
