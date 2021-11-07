19caf0ccb03fc4109a55a44dc6b88da2bcfe7237
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-11-07 12:26:32 -0300

    Implementa carrossel home

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/Models/product.model';
 import { ProductService } from 'src/app/Services/product.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -20,6 +21,36 @@ export class HomeComponent implements OnInit {
 
   sendProduct(product: Product){
     this.productService.receiveProduct(product);
+  }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 600,
+    autoplay: false,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 500,
+    autoplayHoverPause: true,
+    navText: ['<span class="owl-nav">&#8249</span>', '<span class="owl-nav">&#8250;</span>'],
+
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      760: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+    nav: true
   }
 
 }
